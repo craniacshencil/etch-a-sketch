5e1043256b767d270fb2792a44dda09f07fd7e1f
craniacshencil <134303789+craniacshencil@users.noreply.github.com>
2024-01-04 21:05:35 +0530

    Add dynamic sized canvas
    Add button which creates popup and takes input for the no. of pixels in the canvas

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-const container = document.querySelector(".canvasContainer");
-//adding 16x16 grid in the container
-for(let i = 0; i <= 15; i++){
-    const gridRow = document.createElement("div");
-    gridRow.setAttribute("class", "gridRow");
-    container.appendChild(gridRow);
-    for(let j = 0; j <= 15; j++){
-        const gridElement = document.createElement("div");
-        gridElement.setAttribute("class", "gridElement");
-        gridRow.appendChild(gridElement);
+function generateCanvas(dimension){
+    const container = document.querySelector(".canvasContainer");
+    container.replaceChildren();
+    //adding 16x16 grid in the container
+    for(let i = 0; i <= dimension; i++){
+        const gridRow = document.createElement("div");
+        gridRow.setAttribute("class", "gridRow");
+        container.appendChild(gridRow);
+        for(let j = 0; j <= dimension; j++){
+            const gridElement = document.createElement("div");
+            gridElement.setAttribute("class", "gridElement");
+            gridRow.appendChild(gridElement);
+        }
     }
+    const pixels = document.querySelectorAll(".gridElement");
+    pixels.forEach(pixel => {
+        pixel.addEventListener("mouseover", () => pixel.style.backgroundColor = "black");
+    });
 }
 
-//HoverEventListener to add colors
-const pixels = document.querySelectorAll(".gridElement");
-pixels.forEach(pixel => {
-    pixel.addEventListener("mouseover", () => pixel.style.backgroundColor = "black");
+const newCanvasGeneratorButton = document.querySelector(".canvasGenerator");
+newCanvasGeneratorButton.addEventListener("click", () => {
+    const dimension = prompt("Enter the no. of pixels in the grid eg: 10 = 10X10 grid", "");
+    generateCanvas(dimension);
 });
+
+generateCanvas(16);
